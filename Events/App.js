a3ab8f3a4474cd30b042237bbc0580eed9329c19
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-14 10:34:15 +0200

    understood events better

--- Events/App.js
+++ Events/App.js
@@ -1,11 +1,25 @@
-const EventEmitter = require("events");
+const EventEmitter = require("./events");
 
 class Emitter extends EventEmitter {}
 
 const myE = new Emitter();
 
 myE.on("foo", () => {
-  console.log("An event occured.");
+  console.log("An event occured 1.");
+});
+
+myE.on("foo", () => {
+  console.log("An event occured 2.");
+});
+
+myE.on("foo", (x) => {
+  console.log("An event with a prameter occured:");
+  console.log(x);
+});
+
+myE.on("bar", () => {
+  console.log(`An event occured bar.`);
 });
 
 myE.emit("foo");
+myE.emit("foo", "some text");
