ef0ccdd40c5fa4307ede79007045349b1303ce61
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-22 03:20:56 +0200

    I have been able to listen for change even on a file

--- file system/app/app.js
+++ file system/app/app.js
@@ -2,10 +2,13 @@ const fs = require("fs/promises");
 // we would like to first watch changes hapening to the file.
 
 (async () => {
-  const watcher = fs.watch("./"); // the watcher is an asynchronous interator. that is why we had to use a for loop below
+  const watcher = fs.watch("./command.txt"); // the watcher is an asynchronous interator. that is why we had to use a for loop below
 
   // in this for loop, we are looking at the whole director and login all changes to the console.
   for await (const event of watcher) {
-    console.log(event);
+    if (event.eventType === "change") {
+      // The file was changed...
+      console.log("The file was changed");
+    }
   }
 })();
