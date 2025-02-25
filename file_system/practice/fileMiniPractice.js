f0aa92c2f428284b9052470dad66298bb5a363dd
Mario-Gozie <oguedoihucg@gmail.com>
2025-02-26 00:37:02 +0200

    Remove deleted files as part of project update

--- file_system/practice/fileMiniPractice.js
+++
@@ -1,9 +0,0 @@
-// fs module is used in reading a file.
-
-const fs = require("fs");
-
-const content = fs.readFileSync("./text.txt"); // Path for the file to be read
-
-// console.log(content); // Running this code on the console will give hexagonal data and we can convert it to text. remember that each charact takes 8bit or 1byte as it may be called.
-
-console.log(content.toString("utf-8")); // This converts the binary to text.
