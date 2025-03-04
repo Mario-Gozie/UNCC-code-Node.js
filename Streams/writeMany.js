c96458eace7b60dc6d87a8ac57e57904d4a6eec0
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-05 01:01:54 +0200

    done with this part

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -1,14 +1,37 @@
-const fs = require("node:fs/promises");
+// const fs = require("node:fs/promises");
+
+// the code below takes 1 miniute to run,
+// uses 100% of CPU (1 core) and
+// 48MB of the memory
+
+// (async () => {
+//   console.time("writeMany");
+//   // Openening the file
+//   const fileHandle = await fs.open("test.txt", "w");
+
+//   for (let i = 0; i < 1000000; i++) {
+//     // writing into the file
+//     await fileHandle.write(` ${i} `);
+//   }
+
+//   console.timeEnd("writeMany");
+// })();
+
+// the code below takes 10 seconds to run,
+// uses 100% of CPU (1 core) and
+// 48MB of the memory
+
+const fs = require("node:fs");
 
 (async () => {
   console.time("writeMany");
   // Openening the file
-  const fileHandle = await fs.open("test.txt", "w");
+  fs.open("test.txt", "w", (err, fd) => {
+    for (let i = 0; i < 1000000; i++) {
+      const buff = Buffer.from(` ${i} `, "utf-8");
+      fs.writeSync(fd, buff);
+    }
 
-  for (let i = 0; i < 1000000; i++) {
-    // writing into the file
-    await fileHandle.write(` ${i} `);
-  }
-
-  console.timeEnd("writeMany");
+    console.timeEnd("writeMany");
+  });
 })();
