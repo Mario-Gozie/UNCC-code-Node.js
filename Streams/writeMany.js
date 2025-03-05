db84067347bc8a6856d0c2b24ed0e51ef1b80896
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-05 23:15:41 +0200

    practicing streams

--- Streams/writeMany.js
+++ Streams/writeMany.js
@@ -38,32 +38,32 @@
 
 // MAKING THE CODE FASTER WITH STREAMS
 
-const fs = require("node:fs/promises");
+// const fs = require("node:fs/promises");
 
 // Streams make codes run faster but DONT USE STREAMS THIS WAY. It is bad practice
 // the code below takes 1 miniute to run,
 // uses 100% of CPU (1 core) and
 // 48MB of the memory
 
-(async () => {
-  console.time("writeMany");
-  // Openening the file
-  const fileHandle = await fs.open("test.txt", "w");
+// (async () => {
+//   console.time("writeMany");
+//   // Openening the file
+//   const fileHandle = await fs.open("test.txt", "w");
 
-  // creating a write stream of the file
+//   // creating a write stream of the file
 
-  const stream = fileHandle.createWriteStream();
+//   const stream = fileHandle.createWriteStream();
 
-  for (let i = 0; i < 1000000; i++) {
-    // creating a buffer.
-    const buff = Buffer.from(` ${i} `, "utf-8");
-    // writing the buffer into the file.
-    stream.write(buff);
-    await fileHandle.write(` ${i} `);
-  }
+//   for (let i = 0; i < 1000000; i++) {
+//     // creating a buffer.
+//     const buff = Buffer.from(` ${i} `, "utf-8");
+//     // writing the buffer into the file.
+//     stream.write(buff);
+//     await fileHandle.write(` ${i} `);
+//   }
 
-  console.timeEnd("writeMany");
-})();
+//   console.timeEnd("writeMany");
+// })();
 
 /// STREAMS: This is an abstract interface for working with streaming data in Node.js. so this has to do with sending data in chuncks amd this is more efficient than sending everything once.
 
@@ -81,3 +81,46 @@ const fs = require("node:fs/promises");
 // If you have a huge amount of data and you want to write it somewhere, it is adviceable to create a readable stream, read it then pass it to a writeable stream this improves use of memory.
 
 // A writeable stream and a readable steam has only one buffer but a duplex has two. A transform has two too. one for reading and one for writing.
+
+const fs = require("node:fs/promises");
+
+(async () => {
+  console.time("writeMany");
+  // Openening the file
+  const fileHandle = await fs.open("test.txt", "w");
+
+  // creating a write stream of the file
+
+  const stream = fileHandle.createWriteStream();
+
+  console.log(stream.writableHighWaterMark); // This property of a stream gives you the size of a buffer. which is by default (16kb or 16384 bytes)
+
+  // console.log(stream.writableLength); // This show how much of the buffer that is filled. This will be 0 because nothing is written into the buffer yet
+
+  // const buff = Buffer.from("string");
+
+  // allocating 1 megabyte in bytes to my buffer. remember that 8bits make one byte. each bit is either a 1 or 0
+
+  // 1000 bytes = 1 kilobyte
+  // 1000 kilobytes = 1 megabyte
+  const buff = Buffer.alloc(16384, 10); // allocating a size to my buffer. and filling it with 10
+  console.log(stream.write(buff)); //This will return False because we are alocationg values that fill the internal buffer of the stream.
+
+  // console.log(buff);
+
+  setInterval(() => {}, 1000);
+
+  // console.log(stream.writableLength); // this will definitely have a value
+
+  // for (let i = 0; i < 1000000; i++) {
+  //   // creating a buffer.
+  //   const buff = Buffer.from(` ${i} `, "utf-8");
+  //   // writing the buffer into the file.
+  //   stream.write(buff);
+  //   await fileHandle.write(` ${i} `);
+  // }
+
+  // console.timeEnd("writeMany");
+
+  fileHandle.close();
+})();
