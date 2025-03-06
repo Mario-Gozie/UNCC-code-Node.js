e430a9f6dd234b438d38db56e6076a20b57086b2
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-06 19:24:32 +0200

    I have been able to read and write

--- Streams/read-big/readBig.js
+++ Streams/read-big/readBig.js
@@ -1,12 +1,19 @@
 const fs = require("node:fs/promises");
 
 (async () => {
-  const fileHandleRead = await fs.open("test.txt", "r");
+  const fileHandleRead = await fs.open("src.txt", "r");
+  const fileHandleWrite = await fs.open("dest.txt", "w");
 
-  const stream = fileHandleRead.createReadStream({ highWaterMark: 400 });
-
-  stream.on("data", (chunk) => {
-    console.log("---------------");
-    console.log(chunk.length);
+  const streamRead = fileHandleRead.createReadStream({
+    highWaterMark: 64 * 10,
   });
+  const streamWrite = fileHandleWrite.createWriteStream();
+
+  streamRead.on("data", (chunk) => {
+    streamWrite.write(chunk);
+    // console.log("---------------");
+    // console.log(chunk.length);
+  });
+
+  //   stream.on("end", () => {});
 })();
