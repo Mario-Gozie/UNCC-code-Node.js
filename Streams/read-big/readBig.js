e743a967ee740878e341511d0a5a76eeae6af309
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-06 19:15:39 +0200

    specifying the highWaterMark value for a readable stream

--- Streams/read-big/readBig.js
+++ Streams/read-big/readBig.js
@@ -3,9 +3,10 @@ const fs = require("node:fs/promises");
 (async () => {
   const fileHandleRead = await fs.open("test.txt", "r");
 
-  const stream = fileHandleRead.createReadStream();
+  const stream = fileHandleRead.createReadStream({ highWaterMark: 400 });
 
   stream.on("data", (chunk) => {
-    console.log(chunk);
+    console.log("---------------");
+    console.log(chunk.length);
   });
 })();
