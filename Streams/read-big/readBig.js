680363bcc0f5999168931cfab0ff73502c266175
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-07 14:55:32 +0200

    code is better with cleaning the data to make arrays better

--- Streams/read-big/readBig.js
+++ Streams/read-big/readBig.js
@@ -9,8 +9,25 @@ const fs = require("node:fs/promises");
   });
   const streamWrite = fileHandleWrite.createWriteStream();
 
+  let split = "";
+
   streamRead.on("data", (chunk) => {
     const numbers = chunk.toString("utf-8").split("  "); // converting each chunk into an array
+
+    if (Number(numbers[0]) !== Number(numbers[1] - 1)) {
+      if (split) {
+        numbers[0] = split.trim() + numbers[0].trim();
+      }
+    }
+
+    // Taking care of the last element of an array
+    if (
+      Number(numbers[numbers.length - 2]) + 1 !==
+      Number(numbers[numbers.length - 1])
+    ) {
+      split = numbers.pop();
+    }
+
     console.log(numbers);
     if (!streamWrite.write(chunk)) {
       streamRead.pause(); // So what we are saying here is that if the write stream buffer is full, pause reading so no data will be recieved.
