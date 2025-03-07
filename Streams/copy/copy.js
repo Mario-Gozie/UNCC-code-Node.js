3a18ffe5fabfbf59c9677792ca7a2a347e817c94
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-07 16:56:24 +0200

    code is cleaned up better

--- Streams/copy/copy.js
+++ Streams/copy/copy.js
@@ -11,9 +11,24 @@ const fs = require("node:fs/promises");
 
 (async () => {
   console.time("copy");
+  const srcFile = await fs.open("text-small.txt", "r");
   const destFile = await fs.open("text-copy.txt", "w");
-  const result = await fs.readFile("text-small.txt");
+
+  let bytesRead = -1;
+
+  while (bytesRead !== 0) {
+    const readResult = await srcFile.read(); //This would return buffer.
+    bytesRead = readResult.bytesRead;
+
+    if (bytesRead !== 16384) {
+      const indexOfNotFilled = readResult.buffer.indexOf(0); // getting index of first 0
+      const newBuffer = Buffer.alloc(indexOfNotFilled); // allocating the size of the unfilled index to the new buffer
+      readResult.buffer.copy(newBuffer, 0, 0, indexOfNotFilled); // copying a buffer to a new buffer
+      destFile.write(newBuffer);
+    } else {
+      destFile.write(readResult.buffer);
+    }
+  }
 
   console.timeEnd("copy");
-  await destFile.write(result);
 })();
