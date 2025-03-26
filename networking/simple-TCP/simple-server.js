74a3c5d898167f058b8f81bc6a2aba930f76a8bb
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-26 15:12:39 +0200

    leart about TCP today

--- networking/simple-TCP/simple-server.js
+++ networking/simple-TCP/simple-server.js
@@ -15,3 +15,9 @@ server.listen(3099, "127.0.0.1", () => {
 }); // The port to be listened to before firing the server. each application need to have its own port. This is because the requests comming to your computer need to know where to go.
 
 // This server part also takes an IP address and then maybe a function that runs when the server is working.
+
+// TCP requests are used to send infomation you want to be exactly the wy they are like password. without changing it.
+
+// UDP is generally faster than TCP. they are used to streaming activities like video etc. it doesnt care about sending exact data some data may be missing. it can also be used for voice talking facetiming. http request is built on top of UDP and SSH requests are built on top of TCP
+
+// This IP address "127.0.0.1" is known as a loop back address. when an information is sent to it. it still goes back to the computer. it can also be called a local host.
