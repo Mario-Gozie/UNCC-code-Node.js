c934c6b2454673c8a376b8f0bc38a5f551f11ba9
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-27 15:18:40 +0200

    I just created a chat room

--- networking/chat-app/server.js
+++ networking/chat-app/server.js
@@ -2,14 +2,20 @@ const net = require("net");
 
 const server = net.createServer();
 
+// An array of client objects/sockets
+const clients = [];
 // socket simply means, your endpoint
 // this socket is a duplex stream
 server.on("connection", async (socket) => {
   console.log("A new connection to the server.");
 
   socket.on("data", (data) => {
-    socket.write(data);
+    clients.map((s) => {
+      s.write(data);
+    });
   });
+
+  clients.push(socket);
 });
 
 server.listen(3008, "127.0.0.1", () => {
