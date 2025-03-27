1d60710427124f828f06d53ed1805b26e44ff90a
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-27 02:35:22 +0200

    Ive worked been able to create a connection and look at wireshark

--- networking/chat-app/client.js
+++ networking/chat-app/client.js
@@ -1,5 +1,21 @@
 const net = require("net");
 
-const socket = net.createConnection({ host: "127.0.0.1", port: 3008 }, () => {
+const client = net.createConnection({ host: "127.0.0.1", port: 3008 }, () => {
   console.log("connected to the server!");
+});
+
+client.on("end", () => {
+  console.log("Ended!");
+});
+
+// Handle errors on the client socket
+client.on("error", (err) => {
+  //   console.error("Client error:", err);
+
+  // Check for ECONNRESET and handle it separately
+  if (err.code === "ECONNRESET") {
+    console.log("The server has closed the connection unexpectedly.");
+  } else {
+    console.log("An error occurred:", err.message);
+  }
 });
