d072f33eaaab5e84c98aabd689412e84947c82bd
Mario-Gozie <oguedoihucg@gmail.com>
2025-03-31 23:08:05 +0300

    working on trying to name files

--- networking/uploader/client.js
+++ networking/uploader/client.js
@@ -1,21 +1,29 @@
 const net = require("net");
 const fs = require("node:fs/promises");
 
+const socket = net.createConnection({ host: "::1", port: 5050 }, async () => {
+  console.log(process.argv);
+  const filePath = "./test.txt";
+  const fileHandle = await fs.open(filePath, "r");
+  const fileReadStream = fileHandle.createReadStream(); // stream to read from
 
+  // Reading from the source file.
+  fileReadStream.on("data", (data) => {
+    if (!socket.write(data)) {
+      // Checking if the internal buffer of a stream is filled.
+      console.log(`client stream buffer filled/backpressuring`);
+      fileReadStream.pause();
+    }
+  });
 
-const socket = net.createConnection({host:"::1", port: 5050}, async () => { 
-    const filePath = "./text.txt"
-    const fileHandle = await fs.open(filePath, "r");
-    const fileStream = fileHandle.createReadStream()
-    
-    // Reading from the source file.
-    fileStream.on("data", (data) => {
-        socket.write(data);
-    })
+  socket.on("drain", () => {
+    console.log(`client stream buffer drained`);
+    fileReadStream.resume();
+  });
 
-    fileStream.on("end", () => {
-        console.log("The file was successfully uploaded!");
+  fileReadStream.on("end", () => {
+    console.log("The file was successfully uploaded!");
 
-        socket.end();
-    })
-})
+    socket.end();
+  });
+});
--- networking/uploader/server.js
+++ networking/uploader/server.js
@@ -1,38 +1,49 @@
 const net = require("net");
-const fs = require("node:fs/promises")
+const fs = require("node:fs/promises");
 
-const server = net.createServer(()=>{})
+const server = net.createServer(() => {});
 
-let fileHandle, fileStream; // These things are defined there to avoid scope issue. in other words, using it within a function.
+let fileHandle, fileWriteStream; // These things are defined there to avoid scope issue. in other words, using it within a function.
 
+server.on("connection", (socket) => {
+  console.log("New Connection!");
 
-server.on("connection", (socket)=>{
-    console.log("New Connection!");
+  socket.on("data", async (data) => {
+    // Checking if fileHandle is open and if it is I write directly. if it is not, I open before I write.
+    if (!fileHandle) {
+      socket.pause(); // pause recieving data from the client
+      fileHandle = await fs.open(`storage/test.txt`, "w");
+      fileWriteStream = fileHandle.createWriteStream(); // stream to write to
 
-    socket.on("data", async(data)=>{
+      // Writing to our destination file
+      fileWriteStream.write(data);
 
-        // Checking if fileHandle is open and if it is I write directly. if it is not, I open before I write.
-        if(!fileHandle){
-            fileHandle = await fs.open(`storage/test.txt`, "w");
-        fileStream = fileHandle.createWriteStream();
+      socket.resume();
 
+      fileWriteStream.on("drain", () => {
+        console.log(`server stream buffer drained`);
+        socket.resume();
+      });
+    } else {
+      if (!fileWriteStream.write(data)) {
+        // checking if the internal buffer of a writeable stream is filled.
+        console.log(`server stream buffer filled / backpressureing`);
+        socket.pause();
+      }
+    }
+  });
 
-        // Writing to our destination file
-        fileStream.write(data);
-        }else{
-            fileStream.write(data)
-        }
-        
+  // This event runs when the client file ends the socket.
 
-        
-    })
+  socket.on("end", () => {
+    console.log("Connection ended!");
+    // fileHandle = undefined;
+    // fileWriteStream = undefined;
 
-    socket.on("end", ()=>{
-        console.log("Connection ended!")
-        fileHandle.close()
-    })
-})
+    fileHandle.close();
+  });
+});
 
 server.listen(5050, "::1", () => {
-    console.log("Uploader server opened on", server.address())
-})
+  console.log("Uploader server opened on", server.address());
+});
