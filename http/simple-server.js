12fea188c0528b21693e04d4796cc46167840898
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-02 07:12:25 +0300

    was able to set up the server at least

---
+++ http/simple-server.js
@@ -0,0 +1,29 @@
+const http = require("node:http");
+
+// creating the simple server
+
+const server = http.createServer();
+
+server.on("request", (request, response) => {
+  console.log("----------------METHOD: ------------------------");
+  console.log(request.method); // METHODS TELL US  WHAT THE REQUEST IS SUPPOSED TO DO. if it is a get request, it means that it wants to get some data. if it is a post request, it means it wants to post some data to our server. if it is a delete method, it means it wants to delete something from our server.
+
+  console.log("----------------URL: ------------------------");
+  console.log(request.url); // eg if url is "/users" it may have to do with all requests that deals with creating a new user.
+
+  console.log("----------------HEADERS: ------------------------");
+  console.log(request.headers); // headers indicate how the request is structured. eg. saying the body will be json, etc. eg. who sent request, machine the person used. etc. we could add any other extra infomation.
+
+  // The body would not come as a buffer, or text because it will take huge part of our memory. it would come as a stream. This is why we can't access the body as request.body
+
+  // REMEMBER: Request is a readable stream
+  console.log("----------------BODY: ------------------------");
+
+  request.on("data", (chunk) => {
+    console.log(chunk.toString("utf-8"));
+  });
+});
+
+server.listen(8050, () => {
+  console.log(`Server listening on http://localhost:8050`);
+});
