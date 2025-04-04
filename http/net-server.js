eac62f8b7a54506d34248637322221a2b01efe7b
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-04 10:38:24 +0300

    worked on using tcp to send http request

---
+++ http/net-server.js
@@ -0,0 +1,23 @@
+const net = require("net"); // including the net module
+// This is the lowest level of the networking module. all other module are built on top of this. such as the http module, etc.
+
+const server = net.createServer((socket) => {
+  // The socket is actually connections. if any body connects to my server, it will come in as a socket. and socket is a stream and it is a duplex stream. so it can read and write
+
+  socket.on("data", (data) => {
+    // here, the socket listens for data, accepts the data, convert it to string and render it to the console.
+    console.log(data.toString("utf-8"));
+  });
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Opened server on", server.address());
+});
+
+// This server part also takes an IP address and then maybe a function that runs when the server is working.
+
+// TCP requests are used to send infomation you want to be exactly the wy they are like password. without changing it.
+
+// UDP is generally faster than TCP. they are used to streaming activities like video etc. it doesnt care about sending exact data some data may be missing. it can also be used for voice talking facetiming. http request is built on top of UDP and SSH requests are built on top of TCP
+
+// This IP address "127.0.0.1" is known as a loop back address. when an information is sent to it. it still goes back to the computer. it can also be called a local host.
