43156cfcd4152a50821ef6f10011d1ebf399a01b
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-05 20:35:31 +0300

    worked on sending Json and updating inputs

--- http/simple-web/server.js
+++ http/simple-web/server.js
@@ -30,6 +30,34 @@ server.on("request", async (request, response) => {
 
     fileStream.pipe(response); // pipe reads from the readable stream and writes to the response and it automatically handles draining
   }
+
+  if (request.url === "/login" && request.method === "POST") {
+    response.setHeader("Content-Type", "application/json");
+
+    response.statusCode = 200;
+
+    const body = {
+      message: "Logging you in ...",
+    };
+
+    // If you are using encoding chunked, instead of using the file length, then you need to use end to tell node that the item is finished.
+    response.end(JSON.stringify(body));
+  }
+
+  if (request.url === "/user" && request.method === "PUT") {
+    response.setHeader("Content-Type", "application/json");
+
+    response.statusCode = 401;
+
+    const body = {
+      message: "You first have to login.",
+    };
+
+    // If you are using encoding chunked, instead of using the file length, then you need to use end to tell node that the item is finished.
+    response.end(JSON.stringify(body));
+  }
+
+  // upload route
 });
 
 server.listen(9000, () => {
