8e18caa1d7df168f7ad283bad8ff845fbe421031
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-10 23:30:31 +0300

    was able to fix a bug

--- http/poster/server.js
+++ http/poster/server.js
@@ -4,6 +4,20 @@ const PORT = 8000;
 
 const server = new Butter();
 
+// ------------------Files Routes ---------------------------- //
+
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
+server.route("get", "/styles.css", (req, res) => {
+  res.sendFile("./public/styles.css", "text/css");
+});
+
+server.route("get", "/scripts.js", (req, res) => {
+  res.sendFile("./public/scripts.js", "text/javascript");
+});
+
 server.listen(PORT, () => {
   console.log("Server has started on port " + PORT);
 });
