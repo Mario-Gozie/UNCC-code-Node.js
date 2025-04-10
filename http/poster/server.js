619c2a19d17bd7077d7aa1ff6bc1494099d831eb
Mario-Gozie <oguedoihucg@gmail.com>
2025-04-10 23:56:31 +0300

    was able to make my best post

--- http/poster/server.js
+++ http/poster/server.js
@@ -1,5 +1,20 @@
 const Butter = require("../butter");
 
+const USERS = [
+  { id: 1, name: "Liam Brown", username: "liam23", password: "string" },
+  { id: 2, name: "Meredith Green", username: "marit.sky", password: "string" },
+  { id: 3, name: "Ben Adams", username: "ben.poet", password: "string" },
+];
+
+const POST = [
+  {
+    id: 1,
+    title: "This is a post title",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    userId: 1,
+  },
+];
+
 const PORT = 8000;
 
 const server = new Butter();
@@ -16,6 +31,12 @@ server.route("get", "/styles.css", (req, res) => {
 
 server.route("get", "/scripts.js", (req, res) => {
   res.sendFile("./public/scripts.js", "text/javascript");
+});
+
+// --------- JSON ROUTE ---------------------
+
+server.route("get", "/api/posts", (req, res) => {
+  res.status(200).json(POST);
 });
 
 server.listen(PORT, () => {
